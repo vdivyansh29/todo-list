0b4e163d4b60f3b68d2775b64595f04194c491e9
DivyanshVerma <divyanshmotog5g@gmail.com>
2026-05-28 19:14:58 +0530

    Created Navbar and ThemeToggle components

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,11 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1>TO-DO List</h1>
-    </>
+    <div>
+      <h1>
+        TO-DO List
+      </h1>
+    </div>
   )
 }
 
